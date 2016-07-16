718672b206990d1676194078791c0973e8f28fd8
Matt Field <m.field42@gmail.com>
2016-07-16 16:43:00 +0100

    Adds private route table and associations, and subnet outputs 	modified:   vpc/main.tf 	modified:   vpc/outputs.tf

--- vpc/main.tf
+++ vpc/main.tf
@@ -47,3 +47,22 @@ resource "aws_route" "public_internet_gateway" {
 resource "aws_internet_gateway" "main" {
   vpc_id = "${aws_vpc.main.id}"
 }
+
+resource "aws_route_table" "private" {
+  vpc_id = "${aws_vpc.main.id}"
+  tags {
+    Name = "${var.vpc_name}-private"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count = "${length(compact(split(",", var.vpc_public_subnets)))}"
+  subnet_id = "${element(aws_subnet.public.*.id, count.index)}"
+  route_table_id = "${aws_route_table.public.id}"
+}
+
+resource "aws_route_table_association" "private" {
+  count = "${length(compact(split(",", var.vpc_private_subnets)))}"
+  subnet_id = "${element(aws_subnet.private.*.id, count.index)}"
+  route_table_id = "${aws_route_table.private.id}"
+}
