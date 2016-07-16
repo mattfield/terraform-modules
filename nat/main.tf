04b2b15a5c32733c6d20b8ec7764ff53b4494efb
Matt Field <m.field42@gmail.com>
2016-07-16 20:20:57 +0100

    reverts to specifiying no of gateways due to cyclic dep 	modified:   nat/main.tf 	modified:   nat/variables.tf

--- nat/main.tf
+++ nat/main.tf
@@ -4,18 +4,18 @@ provider "aws" {
 
 resource "aws_eip" "nat" {
   vpc = true
-  count = "${length(split(",", var.nat_public_subnet_ids))}"
+  count = "${length(split(",", var.nat_gateway_count))}"
 }
 
 resource "aws_nat_gateway" "nat" {
   allocation_id = "${element(aws_eip.nat.*.id, count.index)}"
   subnet_id = "${element(split(",", var.nat_public_subnet_ids), count.index)}"
-  count = "${length(split(",", var.nat_public_subnet_ids))}"
+  count = "${length(split(",", var.nat_gateway_count))}"
 }
 
 resource "aws_route" "nat_gateway" {
   route_table_id = "${element(split(",", var.nat_private_route_table_ids), count.index)}"
   destination_cidr_block = "0.0.0.0/0"
   nat_gateway_id = "${element(aws_nat_gateway.nat.*.id, count.index)}"
-  count = "${length(split(",", var.nat_private_subnet_ids))}"
+  count = "${length(split(",", var.nat_gateway_count))}"
 }
--- nat/variables.tf
+++ nat/variables.tf
@@ -2,6 +2,7 @@ variable "region" {
   default = "eu-west-1"
 }
 
+variable "nat_gateway_count" {}
 variable "nat_public_subnet_ids" {}
 variable "nat_private_subnet_ids" {}
 variable "nat_private_route_table_ids" {}
