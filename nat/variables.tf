b5489f8cfeaca7486f59b905d9a5bc9f8db1853b
Matt Field <m.field42@gmail.com>
2016-07-16 19:43:29 +0100

    adds variables, main and outputs 	modified:   nat/main.tf 	modified:   nat/outputs.tf 	modified:   nat/variables.tf

---
+++ nat/variables.tf
@@ -0,0 +1,8 @@
+variable "region" {
+  default = "eu-west-1"
+}
+
+variable "nat_gateways_count" {}
+variable "nat_public_subnet_ids" {}
+variable "nat_private_subnet_ids" {}
+variable "nat_route_table_ids" {}
