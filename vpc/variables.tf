5d0056804a02f3961caf5166b746f15f533a1f56
Matt Field <m.field42@gmail.com>
2016-07-16 17:28:43 +0100

    adds empty default for subnets, meaning subnets will not be created if ranges are not specified 	modified:   variables.tf

--- vpc/variables.tf
+++ vpc/variables.tf
@@ -6,9 +6,11 @@ variable "vpc_name" {}
 variable "vpc_cidr_block" {}
 variable "vpc_private_subnets" {
   description = "Comma-delimited list of CIDR ranges for private subnets"
+  default = ""
 }
 variable "vpc_public_subnets" {
   description = "Comma-delimited list of CIDR ranges for public subnets"
+  default = ""
 }
 
 variable "vpc_azs" {}
