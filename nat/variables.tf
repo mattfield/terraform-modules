3a4999a828ea87819be989fee171cffd18ce4b7f
Matt Field <m.field42@gmail.com>
2016-07-16 19:49:28 +0100

    removes superfluous variable 	modified:   nat/variables.tf

--- nat/variables.tf
+++ nat/variables.tf
@@ -2,7 +2,6 @@ variable "region" {
   default = "eu-west-1"
 }
 
-variable "nat_gateways_count" {}
 variable "nat_public_subnet_ids" {}
 variable "nat_private_subnet_ids" {}
 variable "nat_private_route_table_ids" {}
